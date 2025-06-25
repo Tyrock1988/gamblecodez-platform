9764b153697551c82d1acdc81407472194c5f7e3
GAMBLEODEZ aka Tyler <thetylo88@gmail.com>
2025-06-25 00:16:07 -0500

    Delete gamblecodez-deployment directory

--- gamblecodez-deployment/init.sql
+++
@@ -1,7 +0,0 @@
--- Initialize GambleCodez database
-CREATE DATABASE IF NOT EXISTS gamblecodez;
-CREATE USER IF NOT EXISTS gamblecodez_user WITH PASSWORD 'secure_password_change_me';
-GRANT ALL PRIVILEGES ON DATABASE gamblecodez TO gamblecodez_user;
-
--- Enable uuid extension
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
